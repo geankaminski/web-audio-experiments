f77a0e6aad3fa479af9b62ab285bf98f32d1c323
Gean Kaminski <geankaminski93@hotmail.com>
2022-05-16 14:07:53 -0300

    waveform analyser

--- src/js/03-gain.js
+++ src/js/03-gain.js
@@ -4,37 +4,26 @@ let gainNode;
 
 function mousePressed() {
   if (!audioContext) {
-    // Create a new audio context
     audioContext = new AudioContext();
 
-    // Create <audio> tag
     audio = document.createElement("audio");
 
-    // set URL to the MP3 within your Glitch.com assets
     audio.src = "audio/piano.mp3";
 
-    // To play audio through Glitch.com CDN
     audio.crossOrigin = "Anonymous";
 
-    // Enable looping so the audio never stops
     audio.loop = true;
 
-    // Play audio
     audio.play();
 
-    // Create a "Media Element" source node
     const source = audioContext.createMediaElementSource(audio);
 
-    // Create a gain for volume adjustment
     gainNode = audioContext.createGain();
 
-    // wire source to gain
     source.connect(gainNode);
 
-    // wire the gain -> speaker
     gainNode.connect(audioContext.destination);
   } else {
-    // Clean up our element and audio context
     audio.pause();
     audioContext.close();
     audioContext = audio = null;
@@ -50,22 +39,15 @@ function windowResized() {
 }
 
 function draw() {
-  // fill background
   background("black");
 
   fill("white");
   noStroke();
 
-  // Draw play/pause button
   const dim = min(width, height);
   if (audioContext) {
-    // Get a new volume based on mouse position
     const volume = abs(mouseX - width / 2) / (width / 2);
-
-    // Schedule a gradual shift in value with a small time constant
     gainNode.gain.setTargetAtTime(volume, audioContext.currentTime, 0.01);
-
-    // Draw a volume meter
     rectMode(CENTER);
     rect(width / 2, height / 2, dim * volume, dim * 0.05);
   } else {
@@ -73,7 +55,6 @@ function draw() {
   }
 }
 
-// Draw a basic polygon, handles triangles, squares, pentagons, etc
 function polygon(x, y, radius, sides = 3, angle = 0) {
   beginShape();
   for (let i = 0; i < sides; i++) {
--- src/js/04-waveform.js
+++ src/js/04-waveform.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 let audioContext;
 let audioBuffer;
 let analyserNode;
@@ -11,59 +9,44 @@ function mousePressed() {
 }
 
 async function loadSound() {
-  // Re-use the same context if it exists
   if (!audioContext) {
     audioContext = new AudioContext();
   }
 
-  // Re-use the audio buffer as a source
   if (!audioBuffer) {
-    // Fetch MP3 from URL
     const resp = await fetch("audio/chime.mp3");
 
-    // Turn into an array buffer of raw binary data
     const buf = await resp.arrayBuffer();
 
-    // Decode the entire binary MP3 into an AudioBuffer
     audioBuffer = await audioContext.decodeAudioData(buf);
   }
 
-  // Setup a master gain node and AnalyserNode
   if (!gainNode) {
-    // Create a gain and connect to destination
     gainNode = audioContext.createGain();
 
-    // Create an Analyser Node
     analyserNode = audioContext.createAnalyser();
 
-    // Create a Float32 array to hold the data
     analyserData = new Float32Array(analyserNode.fftSize);
 
-    // Connect the GainNode to the analyser
+    // slice go to analyser
     gainNode.connect(analyserNode);
 
-    // Connect GainNode to destination as well
+    // slice go to speaker
     gainNode.connect(audioContext.destination);
   }
 }
 
 async function playSound() {
-  // Snsure we are all loaded up
   await loadSound();
 
-  // Snsure we are in a resumed state
   await audioContext.resume();
 
-  // Now create a new "Buffer Source" node for playing AudioBuffers
   const source = audioContext.createBufferSource();
 
-  // Connect to gain (which will be analyzed and also sent to destination)
   source.connect(gainNode);
 
-  // Assign the loaded buffer
   source.buffer = audioBuffer;
 
-  // Start (zero = play immediately)
   source.start(0);
 }
 
@@ -76,20 +59,17 @@ function windowResized() {
 }
 
 function draw() {
-  // fill background
   background(0, 0, 0);
 
   if (analyserNode) {
     noFill();
     stroke("white");
 
-    // get time domain data
     analyserNode.getFloatTimeDomainData(analyserData);
 
     beginShape();
 
     for (let i = 0; i < analyserData.length; i++) {
-      // -1...1
       const amplitude = analyserData[i];
 
       const y = map(
@@ -109,13 +89,11 @@ function draw() {
   } else {
     fill("white");
     noStroke();
-    // Draw a play button
     const dim = min(width, height);
     polygon(width / 2, height / 2, dim * 0.1, 3);
   }
 }
 
-// Draw a basic polygon, handles triangles, squares, pentagons, etc
 function polygon(x, y, radius, sides = 3, angle = 0) {
   beginShape();
   for (let i = 0; i < sides; i++) {
